be5c4fd14c8effd96c0ca502ef98159c352fe717
Wreid <venere.daniele01@gmail.com>
2026-03-03 10:00:17 +0100

    build

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { createBrowserRouter } from "react-router";
-import Home from "./pages/Home";
-import NotFound from "./pages/not-found";
-import Privacy from "./pages/Privacy";
-
-createRoot(document.getElementById("root")!).render(<App />);
