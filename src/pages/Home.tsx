89aef5947a9177a0a2e615166721ab84ae975910
Wreid <venere.daniele01@gmail.com>
2026-03-11 20:54:14 +0100

    adjusted images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -135,8 +135,61 @@ function Hero2() {
 										</div>
 									</div>
 								</div>
-								<div className="mt-14  pt-24 max-md:pt-0 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
-									<div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+								<div className="mt-14 flex justify-end gap-8 max-lg:gap-0  sm:-mt-44 sm:justify-start  lg:mt-0  lg:pl-0 max-lg:justify-center px-[10rem]  w-full">
+									<div className="ml-auto w-44 max-lg:hidden flex-none space-y-8 pt-32  sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80 ">
+										<div className="relative">
+											<img
+												loading="lazy"
+												alt="incontro con il cliente "
+												src="/studio_zigrino/h1.webp"
+												className="aspect-[2/3] w-full max-lg:w-[90%]  rounded-xl bg-gray-900/5 object-cover shadow-lg dark:bg-gray-700/5"
+											/>
+											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10 dark:ring-white/10" />
+										</div>
+									</div>
+									<div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+										<div className="relative">
+											<img
+												loading="lazy"
+												alt="lavoro in gruppo"
+												src="/studio_zigrino/h2.webp"
+												className="aspect-[2/3] w-full rounded-xl max-lg:w-[90%] bg-gray-900/5 object-cover shadow-lg dark:bg-gray-700/5"
+											/>
+											<div className="pointer-events-none absolute inset-0 rounded-xl md:ring-1 ring-inset ring-gray-900/10 dark:ring-white/10" />
+										</div>
+										<div className="relative">
+											<img
+												loading="lazy"
+												alt="ragazzi discutono un progetto"
+												src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
+												className="aspect-[2/3] w-full rounded-xl max-lg:w-[90%] bg-gray-900/5 object-cover shadow-lg dark:bg-gray-700/5"
+											/>
+											<div className="pointer-events-none absolute inset-0 rounded-xl md:ring-1 ring-inset ring-gray-900/10 dark:ring-white/10" />
+										</div>
+									</div>
+									<div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+										<div className="relative">
+											<img
+												loading="lazy"
+												alt="incontro con la famiglia"
+												src="/studio_zigrino/h3.webp"
+												className="aspect-[2/3] w-full rounded-xl max-lg:w-[90%] bg-gray-900/5 object-cover shadow-lg dark:bg-gray-700/5"
+											/>
+											<div className="pointer-events-none absolute inset-0 rounded-xl md:ring-1 ring-inset ring-gray-900/10 dark:ring-white/10" />
+										</div>
+										<div className="relative">
+											<img
+												loading="lazy"
+												alt="ragazza guarda un parco"
+												src="/studio_zigrino/h4.webp"
+												className="aspect-[2/3] w-full rounded-xl max-lg:w-[90%] bg-gray-900/5 object-cover shadow-lg dark:bg-gray-700/5"
+											/>
+											<div className="pointer-events-none absolute inset-0 rounded-xl md:ring-1 ring-inset ring-gray-900/10 dark:ring-white/10" />
+										</div>
+									</div>
+								</div>
+								{/* <div className="mt-14 flex justify-end gap-8 max-lg:gap-0  sm:-mt-44 sm:justify-start  lg:mt-0  lg:pl-0 max-lg:justify-center px-[10rem]  w-full">
+									<div className="ml-auto w-44 max-lg:hidden flex-none space-y-8 pt-32  sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80 ">
 										<div className="relative">
 											<img
 												alt="incontro con il cliente "
@@ -182,7 +235,7 @@ function Hero2() {
 											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10 dark:ring-white/10" />
 										</div>
 									</div>
-								</div>
+								</div> */}
 							</div>
 						</div>
 					</div>
